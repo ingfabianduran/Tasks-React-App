5903899fffd21aa07796cfb7ccfa5c01e4ec76f7
Fabian Duran <ingfabianavellaneda@gmail.com>
2021-08-24 20:55:50 -0500

    Se realiza el componente ReportTaks

---
+++ src/components/ReportTasks.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Accordion, Badge, Container, Row, Col } from 'react-bootstrap';
+
+function ReportTask() {
+    const TYPES_TASKS = [
+        { id: 1, typeName: 'Incidente', totalCount: 120, countTrue: 60, countFalse: 60 },
+        { id: 2, typeName: 'Requerimiento', totalCount: 150, countTrue: 75, countFalse: 75 },
+    ];
+
+    return (
+        <Accordion defaultActiveKey="0">
+            { TYPES_TASKS.map(item => (
+                <Accordion.Item key={item.id} eventKey={item.id}>
+                    <Accordion.Header >
+                        { item.typeName }
+                    </Accordion.Header>
+                    <Accordion.Body>
+                        <Container>
+                            <Row>
+                                <Col md={4} className="text-center"><h4>Abiertos: <Badge bg="danger">{ item.countTrue }</Badge></h4></Col>
+                                <Col md={4} className="text-center"><h4>Cerrados: <Badge bg="success">{ item.countFalse }</Badge></h4></Col>
+                                <Col md={4} className="text-center"><h4>Totales: <Badge>{ item.countTrue }</Badge> </h4></Col>
+                            </Row>
+                        </Container>
+                    </Accordion.Body>
+                </Accordion.Item>
+            )) }
+        </Accordion>
+    );
+}
+
+export { ReportTask };
